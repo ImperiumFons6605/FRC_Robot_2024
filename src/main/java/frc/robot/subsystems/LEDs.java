09233a8ecb3afd5d963d039bd74f8e42b8d6121c
Carlos Emanuel Salgado Trujillo <111262772+Caryuter@users.noreply.github.com>
2024-02-29 20:19:26 -0600

    Paths (#2)
    * Added subsystems and Pathplanner implementation
    
    * Added Led Subsystem
    
    ---------
    
    Co-authored-by: Caryuter <caryuter162006@gmail.com>

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -1,5 +1,33 @@
 package frc.robot.subsystems;
 
+
+
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
 public class LEDs {
+    Spark ledSpark;
+
     
+    public LEDs (){
+    ledSpark = new Spark (0);
+    }
+
+    public Command setRainbow (){
+        return Commands.runOnce(()->ledSpark.set(-0.97));
+    }
+    
+    public Command INcargo(){
+        return Commands.runOnce(()->ledSpark.set(-0.97));
+    }
+    
+    public Command launched(){
+        return Commands.runOnce(()->ledSpark.set(-0.97));
+    }
+    
+    public Command enemyspoted(){
+        return Commands.runOnce(()->ledSpark.set(-0.97));
+    }
 }
